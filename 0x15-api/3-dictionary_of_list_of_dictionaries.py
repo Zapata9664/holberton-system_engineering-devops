8f614eb0bf376afef993f45cb712e1d5707d30b4
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-09-11 16:13:43 -0500

    Task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,41 +1,38 @@
 #!/usr/bin/python3
-""" Returns information about his/her data list progress. """
-
+""" Gather data from an API """
 import json
+from xmlrpc.client import boolean
 import requests
 from sys import argv
 
 
 def request_api():
-    """Module for do request api"""
-
+    """Gather data from an API"""
     user = "https://jsonplaceholder.typicode.com/todos/"
     var = 1
 
     for count in user:
         todo = "https://jsonplaceholder.typicode.com/todos/?userId=" + str(var)
         user1 = "https://jsonplaceholder.typicode.com/users/" + str(var)
-
         response_user = requests.get(user1)
         user_json = response_user.json()
         name_user = user_json.get("username")
         response_todo = requests.get(todo)
-        todo_json = response_todo.json()
+        json_t = response_todo.json()
         dict = {}
         list_json = []
         if (name_user is None):
             return
-        for index in todo_json:
+        for index in json_t:
             my_dic = {'username': name_user, 'task': '', 'completed': None}
             task_title = index.get('title')
             boolean = index.get('completed')
             my_dic.update(task=task_title, completed=boolean)
             list_json.append(my_dic)
         dict.update({str(var): list_json})
-
         with open("todo_all_employees.json", "a") as file:
             json.dump(dict, file)
-        var += 1
+        var = var + 1
 
 
 if __name__ == "__main__":
