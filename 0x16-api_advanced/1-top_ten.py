9270b61f25197fe01a60646b56447a502379989b
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-09-11 17:14:30 -0500

    Top

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Returns the number of subscribers
+(not active users, total subscribers) for a given subreddit."""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Number of subscribers"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    headers = {"User-Agent": "reddit_user"}
+
+    request = requests.get(url, headers=headers, allow_redirects=False)
+
+    if request.status_code != 200:
+        print('None')
+    else:
+        data = request.json()["data"]
+        list2 = data["children"]
+
+        for post in list2[0:10]:
+            print(post["data"]["title"])
