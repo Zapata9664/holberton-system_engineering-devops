e778563f4194560ea22459c939461dec2a754da2
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-09-11 16:20:28 -0500

    Task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,38 +1,28 @@
 #!/usr/bin/python3
-""" Gather data from an API """
-import json
-import requests
-from sys import argv
+""" Returns information about his/her data list progress. """
 
+from requests import get
+from json import dump
 
-def request_api():
-    """Gather data from an API"""
-    user = "https://jsonplaceholder.typicode.com/todos/"
-    var = 1
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = get(url)
+    users = response.json()
 
-    for count in user:
-        todo = "https://jsonplaceholder.typicode.com/todos/?userId=" + str(var)
-        user1 = "https://jsonplaceholder.typicode.com/users/" + str(var)
-        response_user = requests.get(user1)
-        user_json = response_user.json()
-        name_user = user_json.get("username")
-        response_todo = requests.get(todo)
-        json_t = response_todo.json()
-        dict = {}
-        list_json = []
-        if (name_user is None):
-            return
-        for index in json_t:
-            my_dic = {'username': name_user, 'task': '', 'completed': None}
-            task_title = index.get('title')
-            true_false = index.get('completed')
-            my_dic.update(task=task_title, completed=true_false)
-            list_json.append(my_dic)
-        dict.update({str(var): list_json})
-        with open("todo_all_employees.json", "a") as file:
-            json.dump(dict, file)
-        var = var + 1
-
-
-if __name__ == "__main__":
-    request_api()
+    dict_complete = {}
+    for usr in users:
+        user_id = usr.get('id')
+        username = usr.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+        url = url + '/todos'
+        response = get(url)
+        tasks = response.json()
+        dict_complete[user_id] = []
+        for task in tasks:
+            dict_complete[user_id].append({
+                            "task": task.get('title'),
+                            "completed": task.get('completed'),
+                            "username": username
+                            })
+    with open('todo_all_employees.json', 'w') as f:
+        dump(dict_complete, f)
